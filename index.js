879fc9b7ae5b9f7e4861b46a5d43cd40a9ecd0f3
Dhinesh-07 <sdk11092000@gmail.com>
2023-04-06 12:13:16 +0530

    update in json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
